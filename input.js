dc23e84fd6fee95797e8f91d2cd99505b0e4a8e4
YooSuYeon <usuyn000@gmail.com>
2023-08-28 03:18:06 +0900

    feat: 사용자 입력 운동 횟수 저장
    1 이상, 500 이하의 값을 입력하지 않고 설정 하기 버튼 누른 경우
    - 경고 문구 띄우기
    - 입력한 값 지우기

--- input.js
+++ input.js
@@ -18,8 +18,18 @@ function exerciseButtonHandler () {
   })
 }
 
+const numberRegex = /^((500)|([1-4]\d{2})|([1-9]\d{1})|([1-9]))$/
+
 function settingButtonHandler () {
   let inputExercise = $(".exercise-radio-btn input[type='radio']:checked").val()
+  let inputReps = $("#reps-input input[type='number']").val()
+
+  if (!numberRegex.test(inputReps)) {
+    $('#alert-box').show()
+    $("#reps-input input[type='number']").val('')
+    return
+  }
 
   window.localStorage.setItem('inputExercise', inputExercise)
+  window.localStorage.setItem('inputReps', inputReps)
 }
