055c82e041c9c34c91feb8690dd34cd8f35bfb14
YooSuYeon <usuyn000@gmail.com>
2023-08-28 14:28:11 +0900

    feat: 사용자 입력 세트 수 저장

--- input.js
+++ input.js
@@ -23,6 +23,7 @@ const numberRegex = /^((500)|([1-4]\d{2})|([1-9]\d{1})|([1-9]))$/
 function settingButtonHandler () {
   let inputExercise = $(".exercise-radio-btn input[type='radio']:checked").val()
   let inputReps = $("#reps-input input[type='number']").val()
+  let inputSets = $("#sets-input select[name='sets']").val()
 
   if (!numberRegex.test(inputReps)) {
     $('#alert-box').show()
@@ -32,4 +33,5 @@ function settingButtonHandler () {
 
   window.localStorage.setItem('inputExercise', inputExercise)
   window.localStorage.setItem('inputReps', inputReps)
+  window.localStorage.setItem('inputSets', inputSets)
 }
