5cb1903a8e7e05466d1976cfba59fc1282853d31
GIRL-0 <onpima@naver.com>
2023-08-31 12:36:20 +0900

    refactor: 사용하지 않는 그래프 함수 삭제

--- analysis.js
+++ analysis.js
@@ -2,12 +2,6 @@ window.onload = function () {
   let setNum = Number(window.localStorage.getItem('setNum'))
 
   let avgAccuracy = Number(window.localStorage.getItem('set' + setNum))
-  let minAccuracy = Number(window.localStorage.getItem('minAccuracy'))
-  let maxAccuracy = Number(window.localStorage.getItem('maxAccuracy'))
-
-  fillProgressBar(avgAccuracy, '.avg-bar', '.avg-value', '평균')
-  fillProgressBar(minAccuracy, '.min-bar', '.min-value', '최소')
-  fillProgressBar(maxAccuracy, '.max-bar', '.max-value', '최대')
 
   let setsAccuracy = JSON.parse(window.localStorage.getItem('setsAccuracy'))
   setsAccuracy.push(avgAccuracy)
@@ -15,23 +9,6 @@ window.onload = function () {
 
   let feedbackList = JSON.parse(window.localStorage.getItem('feedbackList'));
   printFeedback(feedbackList)
-}
-
-function fillProgressBar (accuracy, barName, valueName, typeName) {
-  let bar = document.querySelector(barName)
-  let value = document.querySelector(valueName)
-
-  console.log(barName)
-
-  let radius = 54
-  let circumference = 2 * Math.PI * radius
-
-  let dashOffset = circumference * (1 - accuracy / 100)
-
-  value.innerHTML = typeName + '\n' + accuracy + '%'
-
-  bar.style.strokeDashoffset = dashOffset
-  bar.style.strokeDasharray = circumference
 }
 
 function printFeedback (feedbackList) {
